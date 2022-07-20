1220c44268701f148442941dfca30d31abf8b627
CJskii <czareczarek3@gmail.com>
2022-07-21 00:07:19 +0100

    listeners

--- javascript/events/script.js
+++ javascript/events/script.js
@@ -21,3 +21,18 @@ btn.onclick = alertFunction;
 
 // METHOD 3
 btn.addEventListener('click', alertFunction); */
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', function(e){
+    console.log(e)
+})
+
+const btn4 = document.querySelector('#btn4');
+btn4.addEventListener('click', function(e){
+    console.log(e.target);
+})
+
+const btn5 = document.querySelector('#btn5');
+btn5.addEventListener('click', function(e){
+    e.target.style.background = 'blue'
+});
