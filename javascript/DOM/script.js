7a113dab7a34d6ade5de7ce88ed82d7c9a0e0753
CJskii <czareczarek3@gmail.com>
2022-07-20 22:06:28 +0100

    2 elements created

--- javascript/DOM/script.js
+++ javascript/DOM/script.js
@@ -6,3 +6,16 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const paragraph = document.createElement('p');
+paragraph.style.color = 'red'
+paragraph.textContent = "Hey I'm red"
+
+container.appendChild(paragraph);
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = "Hey I'm blue"
+
+container.appendChild(h3);
+
