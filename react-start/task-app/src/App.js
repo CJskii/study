84daa101cc476f96c4a1ea3ec6579d3d897b7226
CJskii <czareczarek3@gmail.com>
2023-01-20 01:53:17 +0000

    editing tasks

--- react-start/task-app/src/App.js
+++ react-start/task-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import uniqid from "uniqid";
 import Overview from "./components/Overview";
 
 class App extends Component {
@@ -11,12 +10,21 @@ class App extends Component {
       task: { text: "", id: this.count },
       tasks: [],
       count: 1,
+      editing: null,
     };
 
     this.deleteTask = this.deleteTask.bind(this);
+
+    this.editTask = this.handleEditTask.bind(this);
+
+    this.editing = this.state.editing;
+
+    this.changeTask = this.handleChangeInTask.bind(this);
+
+    this.saveTask = this.saveEditedTask.bind(this);
   }
 
-  handleChange = (e) => {
+  changeHandler = (e) => {
     this.setState({
       task: {
         text: e.target.value,
@@ -29,7 +37,7 @@ class App extends Component {
     e.preventDefault();
     this.setState({
       tasks: this.state.tasks.concat(this.state.task),
-      task: { text: "", id: this.state.count },
+      task: { text: "" },
       count: this.state.count + 1,
     });
   };
@@ -39,31 +47,48 @@ class App extends Component {
       tasks: this.state.tasks
         .filter((task) => task.id !== id)
         .map((task, index) => {
-          {
-            task.id = index + 1;
-            this.count = index + 1;
-            return task;
-          }
+          task.id = index + 1;
+          return task;
         }),
       count: this.state.count - 1,
     });
   };
 
+  handleEditTask = (id) => {
+    this.setState({
+      editing: id,
+    });
+  };
+
+  handleChangeInTask = () => {};
+
+  saveEditedTask = () => {};
+
   render() {
     const { task, tasks } = this.state;
+
     return (
-      <div>
-        <form onSubmit={this.onSubmitTask} autoComplete="off">
+      <div className="container">
+        <form autoComplete="off" onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
           <input
             type="text"
             id="taskInput"
-            onChange={this.handleChange}
+            onChange={this.changeHandler}
             value={task.text}
           />
-          <button type="submit">Add Task</button>
+          <button type="submit" id="btn">
+            Add task
+          </button>
         </form>
-        <Overview tasks={tasks} deleteTask={this.deleteTask} />
+        <Overview
+          deleteTask={this.deleteTask}
+          editTask={this.editTask}
+          editing={this.editing}
+          handleEditTask={this.changeTask}
+          saveTask={this.saveTask}
+          tasks={tasks}
+        />
       </div>
     );
   }
--- react-start/task-app/src/components/Overview.js
+++ react-start/task-app/src/components/Overview.js
@@ -1,21 +1,51 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrashCan,
+  faPenToSquare,
+  faListCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Overview = (props) => {
-  const { tasks, deleteTask } = props;
+  const {
+    tasks,
+    deleteTask,
+    handleEditTask,
+    editing,
+    handleChangeInTask,
+    saveEditedTask,
+  } = props;
 
   return (
     <ul>
       {tasks.map((task) => {
         return (
           <li key={task.id}>
-            {task.id}. {task.text}
-            <FontAwesomeIcon
-              key={task.id}
-              icon={faTrashCan}
-              onClick={() => deleteTask(task.id)}
-            />
+            {editing === task.id ? (
+              <>
+                <input
+                  type="text"
+                  defaultValue={task.text}
+                  onBlur={(e) => handleChangeInTask(task.id, e.target.value)}
+                />
+                <FontAwesomeIcon
+                  icon={faListCheck}
+                  onClick={() => saveEditedTask(task.id)}
+                />
+              </>
+            ) : (
+              <>
+                {task.id}. {task.text}
+                <FontAwesomeIcon
+                  icon={faTrashCan}
+                  onClick={() => deleteTask(task.id)}
+                />
+                <FontAwesomeIcon
+                  icon={faPenToSquare}
+                  onClick={() => handleEditTask(task.id)}
+                />
+              </>
+            )}
           </li>
         );
       })}
