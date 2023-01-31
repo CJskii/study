14a1b4955689c0bd2ea221d8ff1eb42cf7ddb3a9
CJskii <czareczarek3@gmail.com>
2023-01-31 22:23:43 +0000

    Lifecycle methods

--- react-start/lifecycle/src/App.js
+++ react-start/lifecycle/src/App.js
@@ -1,8 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
+import Counter from "./Counter";
 
-function App() {
-  return <div className="app"></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mount: true,
+      ignoreProp: 0,
+      seed: 40,
+    };
+
+    this.mountCounter = () => this.setState({ mount: true });
+    this.unmountCounter = () => this.setState({ mount: false });
+
+    this.ignoreProp = () => this.setState({ ignoreProp: Math.random() });
+    this.seedGenerator = () =>
+      this.setState({ seed: Number.parseInt(Math.random() * 100) });
+  }
+  render() {
+    return (
+      <div className="app">
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount
+        </button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+          Unmount
+        </button>
+        <button onClick={this.ignoreProp} disabled={!this.state.mount}>
+          Ignore Prop
+        </button>
+        <button onClick={this.seedGenerator}>Generate Seed</button>
+        {this.state.mount ? (
+          <Counter ignoreProp={this.state.ignoreProp} seed={this.state.seed} />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
